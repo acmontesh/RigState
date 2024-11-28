1648391b8fd747106e4015beaddea21e67666b31
Abraham Montes <acmontesh@gmail.com>
2024-11-28 14:07:01 -0600

    correcting quotes in main for batch processing

--- server/main.py
+++ server/main.py
@@ -8,7 +8,7 @@ batch       = [
 
 if __name__=='__main__':
     for i,ba in enumerate( batch ):
-        print( f"[MODEL DESCRIPTION]...................HEADS: {ba["nHeads"]} | LAYERS: {ba["nLayers"]} | CW: {ba["slidingWindowCoverage"]}." )
+        print( f"[MODEL DESCRIPTION]...................HEADS: {ba['nHeads']} | LAYERS: {ba['nLayers']} | CW: {ba['slidingWindowCoverage']}." )
         trainer = Trainer(  )
         trainer.loadData( r"C:\Users\abrah\OneDrive\Doctorate Petroleum Engineering\0A. RESEARCH PROJECT\16. RIG ACTIVITY ENGINE\0. DATA\2. ANNOTATED TRAINING DATA" )
         trainLosses, model=trainer.trainModel( "transformer",batchSize=1024,nEpochs=80,slidingWindow=[5,30],slidingWindowCoverage=ba["slidingWindowCoverage"],nHeads=ba["nHeads"],nLayers=ba["nLayers"] )

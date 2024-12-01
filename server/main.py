48961a11b6c12b2a46a964370341e0ab81ba1e17
Abraham C Montes <acmontesh@gmail.com>
2024-12-01 17:01:37 -0600

    update from jupiter

--- server/main.py
+++ server/main.py
@@ -2,9 +2,13 @@ from Trainer import Trainer
 
 batch       = [
 #    dict(slidingWindow=[2,30],slidingWindowCoverage=30,nHidden=50,nLayers=3),
-    dict(slidingWindow=[2,5,10,30],slidingWindowCoverage=15,nHeads=8,nLayers=6),
-    dict(slidingWindow=[2,5,10,30],slidingWindowCoverage=30,nHeads=8,nLayers=6),
-    dict(slidingWindow=[2,5,10,30],slidingWindowCoverage=60,nHeads=8,nLayers=6),
+    dict(slidingWindow=[2,30],slidingWindowCoverage=500,nHeads=8,nLayers=3),
+    dict(slidingWindow=[2,30],slidingWindowCoverage=15,nHeads=8,nLayers=3),
+    dict(slidingWindow=[2,30],slidingWindowCoverage=30,nHeads=8,nLayers=6),
+#    dict(slidingWindow=[2,30],slidingWindowCoverage=25,nHeads=8,nLayers=6),
+#    dict(slidingWindow=[3,30],slidingWindowCoverage=15,nHeads=8,nLayers=6),
+#    dict(slidingWindow=[3,30],slidingWindowCoverage=20,nHeads=8,nLayers=6),
+#    dict(slidingWindow=[3,30],slidingWindowCoverage=20,nHeads=8,nLayers=6)
  #   dict(slidingWindow=[2,30],slidingWindowCoverage=30,nHidden=50,nLayers=5),
  #   dict(slidingWindow=[2,30],slidingWindowCoverage=60,nHidden=100,nLayers=5),
  #   dict(slidingWindow=[5,30],slidingWindowCoverage=60,nHidden=100,nLayers=5),
@@ -23,5 +27,5 @@ if __name__=='__main__':
         #print( f"[MODEL DESCRIPTION]...................HIDDEN NEURONS: {ba['nHidden']} | LAYERS: {ba['nLayers']} | CW: {ba['slidingWindowCoverage']}." )
         trainer = Trainer(  )
         trainer.loadData( "/home/acmontesh/Data" )
-        trainLosses, model=trainer.trainModel( "conv_transformer",batchSize=1024,nEpochs=80,slidingWindow=ba["slidingWindow"],slidingWindowCoverage=ba["slidingWindowCoverage"],nHeads=ba["nHeads"],nLayers=ba["nLayers"],nInputs=14 )
-        #trainLosses, model=trainer.trainModel( "conv_lstm",batchSize=1024,nEpochs=80,slidingWindow=ba["slidingWindow"],slidingWindowCoverage=ba["slidingWindowCoverage"],nHidden=ba["nHidden"],nLayers=ba["nLayers"] )
+        trainLosses, model=trainer.trainModel( "transformer",batchSize=1024,nEpochs=80,slidingWindow=ba["slidingWindow"],slidingWindowCoverage=ba["slidingWindowCoverage"],nHeads=ba["nHeads"],nLayers=ba["nLayers"],nInputs=10 )
+         #trainLosses, model=trainer.trainModel( "conv_lstm",batchSize=1024,nEpochs=80,slidingWindow=ba["slidingWindow"],slidingWindowCoverage=ba["slidingWindowCoverage"],nHidden=ba["nHidden"],nLayers=ba["nLayers"] )
